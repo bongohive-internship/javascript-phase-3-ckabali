20ea8e7c38a93e569a8e9a5e221eda3c38a1ac6d
collins kabali <51477747+ckabali@users.noreply.github.com>
2019-06-14 15:38:55 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,10 @@
 // solutions to the exercises mentioned in the readme should be in this file.
+var sum = 0;
+for (var x = 0; x < 1000; x++)
+{
+    if (x % 3 === 0 || x % 5 === 0)
+    {
+       sum += x;
+    }
+}
+console.log(sum);
